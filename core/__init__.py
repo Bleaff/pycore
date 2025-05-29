2b691c7637d2f2a6ef3722b499dc1e1d97924b3a
kotovanu <marius-7@mail.ru>
2025-05-29 17:31:59 +0300

    test

--- core/__init__.py
+++ core/__init__.py
@@ -1,4 +1,4 @@
-"""Core functionality for the application.
+"""Core functionality for the application
 
 This module contains the main logic and essential components required to run the application.
 It provides foundational classes, functions, and utilities that are used throughout the project.
